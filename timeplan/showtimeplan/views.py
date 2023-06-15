7bed88b4a57696ee94b1039666ede121d0bd6f39
Friedrich <johndoe@example.com>
2023-06-15 23:43:33 +0100

    Ajout des pages de connexion et d'enregistrement

--- timeplan/showtimeplan/views.py
+++ timeplan/showtimeplan/views.py
@@ -4,10 +4,20 @@ from showtimeplan.models import User
 def formreg(request):
     return render(request, 'showtimeplan/index.html')
 def insertuser(request):
-    vunom=request.POST['nom'];
-    vuprenom = request.POST['prenom'];#LES element entre crochets sont les nom de input dans le template
-    vuemail = request.POST ['Email'];
-    vuborn = request.POST['borndate'];
-    us=User(nom = vunom,prenom = vuprenom, Email = vuemail,date_de_naissance=vuborn);
+    vunom=request.POST['nom']
+    vuprenom = request.POST['prenom'] #Les élements entre crochets sont les noms des input dans le template
+    vuemail = request.POST ['Email']
+    vuborn = request.POST['borndate']
+    us=User(nom = vunom,prenom = vuprenom, Email = vuemail,date_de_naissance=vuborn)
     us.save()
     return render(request,'showtimeplan/index.html',{})
+
+def index(request):
+    return render(request, "showtimeplan/index_commite.html")
+
+def login(request):
+    return render(request, "showtimeplan/login.html")
+
+def register(request):
+    return render(request, "showtimeplan/register.html")
+
