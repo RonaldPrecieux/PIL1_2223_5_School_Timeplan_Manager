f462b9c95ad1386c76dfa5f0b9b5d827bc674474
Friedrich <johndoe@example.com>
2023-06-16 19:33:27 +0100

    Création de base de données liée au formulaire d'enregistrement

--- timeplan/showtimeplan/admin.py
+++ timeplan/showtimeplan/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from showtimeplan.models import User
 
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('nom', 'prenom', 'Email') # liste les champs que nous voulons sur l'affichage de la liste
+    list_display = ('nom', 'prenom', 'email', 'numero_telephone', 'mot_de_passe', 'confirmer_mot_de_passe') # liste les champs que nous voulons sur l'affichage de la liste
 admin.site.register(User, UserAdmin) # nous modifions cette ligne, en ajoutant un deuxième argument
--- timeplan/showtimeplan/models.py
+++ timeplan/showtimeplan/models.py
@@ -1,9 +1,17 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 class User(models.Model):
-    nom = models.fields.CharField(max_length=100)
-    prenom = models.fields.CharField(max_length=100)
-    date_de_naissance = models.fields.DateField()
-    Email=models.fields.EmailField()
+    nom = models.fields.CharField(max_length=100, default ="")
+    prenom = models.fields.CharField(max_length=100, default ="")
+    email = models.EmailField(unique=True, default="")
+    numero_telephone = models.CharField(max_length=20, default= 555)
+    mot_de_passe = models.CharField(max_length=128, default ="")
+    confirmer_mot_de_passe = models.CharField(max_length=128, editable=False, default ="")
+    
     class Meta:
         db_table="etudiants"
+
+    """def save(self, *args, **kwargs):
+        # Hacher le mot de passe avant de l'enregistrer
+        self.mot_de_passe = make_password(self.mot_de_passe)
+        super().save(*args, **kwargs)"""
